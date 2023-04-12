44c570421773efd6aa29a29cc12df21ad1c4dadb
vgacable <vid.grujic@gmail.com>
2023-04-12 13:44:57 -0400

    Should be working feature extraction, need to still choose window size

--- featext.py
+++ featext.py
@@ -9,15 +9,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.inspection import DecisionBoundaryDisplay 
 from sklearn.decomposition import PCA
 
-features = norm_train.DataFrame(columns=['mean', 'median', 'std', 'max', 'variance'])
+features = norm_train.DataFrame(columns=['mean', 'median', 'std', 'max', 'min', 'range', 'variance', 'kurtosis', 'skew', 'mode'])
 
 # window size
 window_size = 125
 
 max_index = 1000 # Sub in for real value
 
-features['mean'] = norm_train[max_index, :-1].rolling(window=window_size).mean()
-features['median'] = norm_train[max_index, :-1].rolling(window=window_size).median()
-features['std'] = norm_train[max_index, :-1].rolling(window=window_size).std()
-features['max'] = norm_train[max_index, :-1].rolling(window=window_size).max()
-features['variance'] = norm_train[max_index, :-1].rolling(window=window_size).var()
+features['mean'] = norm_train[1, : 4].rolling(window=window_size).mean()
+features['median'] = norm_train[1, : 4].rolling(window=window_size).median()
+features['std'] = norm_train[1, : 4].rolling(window=window_size).std()
+features['max'] = norm_train[1, : 4].rolling(window=window_size).max()
+features['min'] = norm_train[1, : 4].rolling(window=window_size).min()
+features['range'] = norm_train[1, : 4].rolling(window=window_size).range()
+features['variance'] = norm_train[1, : 4].rolling(window=window_size).var()
+features['kurtosis'] = norm_train[1, : 4].rolling(window=window_size).kurt()
+features['skew'] = norm_train[1, : 4].rolling(window=window_size).skew()
+features['mode'] = norm_train[1, : 4].rolling(window=window_size).mode()
