69c7e550bf65668f49745db54c50380c26bf9e07
vgacable <vid.grujic@gmail.com>
2023-04-13 16:29:21 -0400

    currently feature extract added to general program

--- connectFuncs.py
+++ connectFuncs.py
@@ -1,9 +1,16 @@
 from prepro import prepro
 from prepro2 import prepro2
+from featext import featureExtract
 
 # Generate original model:
 
-trainpre = prepro("data.h5", "Train")
-testpre = prepro("data.h5", "Test")
+train_pre = prepro("data.h5", "Train")   # [0] is norm_train_X, [1] is y_train
+test_pre = prepro("data.h5", "Test")     # [0] is norm_test_X, [1] is y_test
 
-csvtest = prepro2("xxxxx.csv")
+train_features = featureExtract(train_pre) # feature extraction of training set
+test_features = featureExtract(test_pre) # feature extraction of test set
+
+
+
+csv_test_pre = prepro2("xxxxx.csv")          # [0] is norm_csvtest_X, [1] is y_csvtest
+csv_test_features = featureExtract(csv_test_pre) # feature extraction of csv test set
