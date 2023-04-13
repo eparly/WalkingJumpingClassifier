1476f1bc4fb5a6f80784607bc68a9391707550c5
vgacable <vid.grujic@gmail.com>
2023-04-13 16:15:55 -0400

    fix for prepro2 func

--- connectFuncs.py
+++ connectFuncs.py
@@ -1,8 +1,9 @@
 from prepro import prepro
+from prepro2 import prepro2
 
 # Generate original model:
 
 trainpre = prepro("data.h5", "Train")
 testpre = prepro("data.h5", "Test")
 
-csvtest = prepro("xxxxx.csv")
+csvtest = prepro2("xxxxx.csv")
